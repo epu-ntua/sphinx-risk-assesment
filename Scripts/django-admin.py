56588617e58031ca72dc302395b1597d424f35cc
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-02-21 19:12:11 +0200

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Michael Kontoulis\PycharmProjects\RiskAssessmentFlask\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
