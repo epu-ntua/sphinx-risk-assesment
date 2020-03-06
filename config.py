d3dbe0f513190551e0a23e146fb6cfdf64d2b90a
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-03-06 20:32:38 +0200

    -Setup database plugins -Setup bootstrap plugins -Added custom error HTML -Added bootstrap as base for base html -Added custom base html with navbar -Updated config -Updated routes -Added models for CVE, CWE , CAPEC

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "LwI3vO0NijuXzWdTQCEsyGmkucBja5dcDPREFuTMMXARlYwKohLLqvGSw8687pJ0"
 
-    SQLALCHEMY_DATABSE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+                             # + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
