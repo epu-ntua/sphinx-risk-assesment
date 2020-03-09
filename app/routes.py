ecc613eb2d9b7b65d5fc260c38d93f71a92fe0d4
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-03-09 17:11:11 +0200

    -Added database

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
-@app.route('/')
-@app.route('/dashboard')
 
+@app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
