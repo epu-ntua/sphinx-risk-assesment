b910655e30d738af27030f7dc7d49689d1b9f672
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-03-11 18:37:11 +0200

    -Added fronted stuff

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 from app import app
 from flask import render_template
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/')
+@app.route('/home')
+def entry_page():
+    return render_template('entry_page.html')
+
+@app.route('/dashboardCorrelate')
+def dashboardAssetThreat():
+    return render_template('dashboardCorrelate.html')
 
 @app.route('/test_gd')
 def test_gd():
