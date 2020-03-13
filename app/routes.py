f9f16c103225fe3cd065f2acbac923d197beda51
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-03-13 14:42:58 +0200

    -Added all frontend material -Missing front end logic

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,17 @@ from flask import render_template
 def entry_page():
     return render_template('entry_page.html')
 
-@app.route('/dashboardCorrelate')
-def dashboardAssetThreat():
-    return render_template('dashboardCorrelate.html')
+@app.route('/assets')
+def assets():
+    return render_template('assets.html')
+
+@app.route('/assets/1/vulnerabilities')
+def vulnerabilities():
+    return render_template('vulnerabilities.html')
+
+@app.route('/assets/1/vulnerabilities/1/threats')
+def threats():
+    return render_template('threats.html')
 
 @app.route('/test_gd')
 def test_gd():
