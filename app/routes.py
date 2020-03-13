1cda31791152e90694383b98866d32af413049d8
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-03-13 20:14:04 +0200

    -Added frontend logic in assets.html -Other pages jsut need replication same logic apllies more or less -Missing backend communication

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,45 @@
 from app import app
-from flask import render_template
+from flask import render_template,request ,redirect
+import flask
 
 @app.route('/')
 @app.route('/home')
 def entry_page():
     return render_template('entry_page.html')
 
-@app.route('/assets')
-def assets():
-    return render_template('assets.html')
+@app.route('/assets/' ,defaults={"asset": -1})
+@app.route('/assets/<int:asset>/' , methods=['GET', 'POST'])
+def assets(asset):
+    if request.method == 'POST':
+        if asset != -1:
+            print(request.form)
 
-@app.route('/assets/1/vulnerabilities')
-def vulnerabilities():
-    return render_template('vulnerabilities.html')
+            toRedirect = "vulnerabilities/"
+            return redirect( toRedirect)
+        else:
+            return redirect("/assets/")
+    else:
+        assetsArray = [ 1 , 2 ,3 ]
+        proposedCVEArray = [ 1 , 2 ,3 ]
+        othersCVEArray = [ 4 , 5 ]
 
-@app.route('/assets/1/vulnerabilities/1/threats')
-def threats():
-    return render_template('threats.html')
+        return render_template('assets.html', asset = asset , assets = assetsArray , proposedCVEArray = proposedCVEArray ,othersCVEArray = othersCVEArray)
+
+@app.route('/assets/<int:asset>/vulnerabilities/', defaults={"asset": -1 ,"vulnerability" : -1})
+@app.route('/assets/<int:asset>/vulnerabilities/<int:vulnerability>/' , methods=['GET', 'POST'])
+def vulnerabilities(asset, vulnerability):
+    if request.method == 'POST':
+        i = 5
+    else:
+        return render_template('vulnerabilities.html', asset = asset, vulnerability = vulnerability)
+
+@app.route('/assets/<int:asset>/vulnerabilities/<int:vulnerability>/threats/' , defaults={"asset": -1 ,"vulnerability" : -1, "threat": -1})
+@app.route('/assets/<int:asset>/vulnerabilities/<int:vulnerability>/threats/<int:threat>/' , methods=['GET', 'POST'])
+def threats(asset, vulnerability,threat):
+    if request.method == 'POST':
+        i = 5
+    else:
+        return render_template('threats.html' ,asset = asset, vulnerability = vulnerability , threat = threat )
 
 @app.route('/test_gd')
 def test_gd():
