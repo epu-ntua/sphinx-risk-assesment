2e6386492d8edf89caebc82483891eecc91cc8d6
Michael Kontoulis <m.kontoulis.cs@gmail.com>
2020-07-07 16:32:19 +0300

    -Frontend Upgrades

--- app/routes.py
+++ app/routes.py
@@ -74,6 +74,62 @@ def gira_overview():
     else:
         return render_template('gira_overview.html')
 
+@app.route('/gira_overview/gira_threat_exposure/' , methods=['GET', 'POST'])
+def gira_threat_exposure():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_threat_exposure.html')
+
+@app.route('/gira_overview/gira_threat_response/' , methods=['GET', 'POST'])
+def gira_threat_response():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_threat_response.html')
+
+@app.route('/gira_overview/gira_threat_materialisation/' , methods=['GET', 'POST'])
+def gira_threat_materialisation():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_threat_materialisation.html')
+
+@app.route('/gira_overview/gira_consequence/' , methods=['GET', 'POST'])
+def gira_consequence():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_consequence.html')
+
+@app.route('/gira_overview/gira_asset_status/' , methods=['GET', 'POST'])
+def gira_asset_status():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_asset_status.html')
+
+@app.route('/gira_overview/gira_asset_impact/' , methods=['GET', 'POST'])
+def gira_asset_impact():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_asset_impact.html')
+
+@app.route('/gira_overview/gira_objective/' , methods=['GET', 'POST'])
+def gira_objective():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_objective.html')
+
+@app.route('/gira_overview/gira_result/' , methods=['GET', 'POST'])
+def gira_result():
+    if request.method == 'POST':
+        return redirect("/gira_overview/")
+    else:
+        return render_template('gira_result.html')
+
 
 @app.route('/test_gd')
 def test_gd():
