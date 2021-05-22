1b9f032b28c98428775bbb1a1aeb747c43ec1912
@mkontoulis <mkontoulis@epu.ntua.gr>
2021-05-22 18:52:22 +0300

    -Working ML and RCRA on same docker-compose -ML removed mount -Git clone in run-all.sh is commented -In run-all.sh removed extra model algorithms to save time during testing. Should be re enabled at the end

--- MLflow/run_app/run_all.sh
+++ MLflow/run_app/run_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
-echo "Cloning VCDB ..."
-(git clone https://github.com/vz-risk/VCDB.git temp \
-&& mv temp/.git ../VCDB/.git) \
-|| echo "Database update failed, proceeding with stored VCDB"
-rm -rf temp
-echo "Proceeding to training..."
-./run_app/train_attribute.sh "LGBM RF KNN"
-./run_app/train_asset.sh "LGBM RF KNN"
-./run_app/train_action.sh "LGBM RF KNN" 
+#echo "Cloning VCDB ..."
+#(git clone https://github.com/vz-risk/VCDB.git temp \
+#&& mv temp/.git ../VCDB/.git) \
+#|| echo "Database update failed, proceeding with stored VCDB"
+#rm -rf temp
+#echo "Proceeding to training..."
+#./run_app/train_attribute.sh "LGBM RF KNN"
+#./run_app/train_asset.sh "LGBM RF KNN"
+#./run_app/train_action.sh "LGBM RF KNN"
+./run_app/train_attribute.sh "LGBM"
+./run_app/train_asset.sh "LGBM"
+./run_app/train_action.sh "LGBM"
 tail -F "KeepingContainerAlive"
