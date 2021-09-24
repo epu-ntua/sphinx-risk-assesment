7de699b297290a6522d24bf54590fc6e659d62d4
@mkontoulis <mkontoulis@epu.ntua.gr>
2021-09-24 20:26:27 +0300

    Risk Dashboard initial functionality

--- app/routes/routes_dashboard.py
+++ app/routes/routes_dashboard.py
@@ -1,10 +1,15 @@
 from flask import render_template, request, redirect, jsonify, Response, flash
 from multiprocessing import Process
+
+from sqlalchemy.exc import SQLAlchemyError
+
 from app.producer import *
 from app.globals import *
 from app.utils import *
 from app.forms import *
 from app import app
+from app.utils.utils_database import convert_database_items_to_json_table
+
 
 @app.route('/repo/dashboard/asset/', methods=['GET', 'POST'])
 def repo_dashboard_asset():
@@ -51,9 +56,9 @@ def repo_dashboard_threat():
         return render_template('templates_dashboard/repo_threat_dashboard.html', repo_threats=repo_threats)
 
 
-@app.route('/repo/dashboard/risk/objectives/', methods=['GET', 'POST'])
+@app.route('/repo/dashboard/risk/objectives/threat/<threat_id>/asset/<asset_id>/', methods=['GET', 'POST'])
 # @app.route('/repo/dashboard/risk/objectives/threat/<threat_id>/asset/<asset_id>/', methods=['GET', 'POST'])
-def repo_dashboard_risk_objectives():
+def repo_dashboard_risk_objectives(threat_id=1, asset_id=-1):
     if request.method == 'POST':
         return redirect("/repo/dashboard/risk/objectives/")
     else:
@@ -61,6 +66,67 @@ def repo_dashboard_risk_objectives():
         # if assetsArray != -1:
         #     return render_template('asset_dashboard.html', assets=assetsArray)
         # else:
+        try:
+            these_threats = RepoThreat.query.all()
+        except SQLAlchemyError:
+            return Response("SQLAlchemyError", 500)
+
+        try:
+            these_assets = RepoAsset.query.all()
+        except SQLAlchemyError:
+            return Response("SQLAlchemyError", 500)
+
+        try:
+            this_asset = RepoAsset.query.filter_by(id=asset_id).all()
+        except SQLAlchemyError:
+            return Response("SQLAlchemyError", 500)
+
+        try:
+            this_threat = RepoThreat.query.filter_by(id=threat_id).all()
+        except SQLAlchemyError:
+            return Response("SQLAlchemyError", 500)
+
+        this_threat = convert_database_items_to_json_table(this_threat)
+        these_threats = convert_database_items_to_json_table(these_threats)
+        these_assets = convert_database_items_to_json_table(these_assets)
+
+        if threat_id != -1 and asset_id != -1:
+            try:
+                this_exposure = RepoAssetRepoThreatRelationship.query.filter_by(repo_threat_id=threat_id).first()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+            try:
+                these_materialisations = RepoMaterialisation.query.filter_by(threat_id=threat_id).all()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+            try:
+                these_consequences = RepoConsequence.query.filter_by(threat_id=threat_id).all()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+            try:
+                these_services = RepoService.query.filter(RepoService.assets.any(id=asset_id)).all()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+            try:
+                these_impacts = RepoImpact.query.all()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+            try:
+                these_objectives = RepoObjective.query.all()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+            try:
+                these_utils = RepoUtility.query.all()
+            except SQLAlchemyError:
+                return "SQLAlchemyError"
+
+
         repo_threats = [
             {
                 "likelihood": "Certain",
@@ -104,7 +170,11 @@ def repo_dashboard_risk_objectives():
             }
         ]
         print(repo_threats)
-        return render_template('templates_dashboard/repo_risk_objectives_dashboard.html', repo_threats=repo_threats)
+        print(this_threat)
+        return render_template('templates_dashboard/repo_risk_objectives_dashboard.html', repo_threats=repo_threats,
+                               these_threats=these_threats, threat_id=threat_id, asset_id=asset_id,
+                               this_threat=this_threat, these_assets=these_assets, this_asset=this_asset
+                               )
 
 
 @app.route('/repo/dashboard/vulnerability/', methods=['GET', 'POST'])
@@ -128,4 +198,5 @@ def repo_dashboard_vulnerability():
             }
         ]
         print(repo_vulnerabilities)
-        return render_template('templates_dashboard/repo_vulnerability_dashboard.html', repo_vulnerabilities=repo_vulnerabilities)
+        return render_template('templates_dashboard/repo_vulnerability_dashboard.html',
+                               repo_vulnerabilities=repo_vulnerabilities)
