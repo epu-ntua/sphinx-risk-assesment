cbe667d9a2db3d276aaf225313aa3aa6e71df897
@mkontoulis <mkontoulis@epu.ntua.gr>
2021-10-27 15:31:04 +0100

    -New risk assessment yaml files

--- app/globals.py
+++ app/globals.py
@@ -1,3 +1,8 @@
-serverPort = '5002'
-serverAddress = '0.0.0.0'
+import os
+
+GLOBAL_SERVER_PORT = os.environ.get('GLOBAL_SERVER_PORT') if os.environ.get('GLOBAL_SERVER_PORT') else "5002"
+GLOBAL_SERVER_ADDRESS = os.environ.get('GLOBAL_SERVER_ADDRESS') if os.environ.get('GLOBAL_SERVER_ADDRESS') else "127.0.0.1"
+
+serverPort = GLOBAL_SERVER_PORT
+serverAddress = GLOBAL_SERVER_ADDRESS
 # serverAddress = '0.0.0.0'
