714d0fdc19baca15b09bba1315e8486a2e395f20
mkontoulis <mkontoulis@epu.ntua.gr>
2022-02-16 18:11:09 +0200

    -New static info missing -Info import fix

--- app/utils/utils_database.py
+++ app/utils/utils_database.py
@@ -1,6 +1,8 @@
 from app.models import *
 import json
 import os
+from datetime import datetime
+from dateutil import parser
 
 def convert_database_items_to_json_table(items):
     """ Converts sqlalchemy entries to json ,
@@ -169,24 +171,116 @@ def rcra_db_init():
     # Adding preset Assets
     to_add_assets = import_fixture_from_file("repo_asset")
     for asset in to_add_assets:
-        to_add_asset = RepoAsset(**asset)
+        tempasset = asset
+        print("--------TEMP ASSET VALUES IS --------")
+        print(tempasset)
+        if tempasset["verified"] == "":
+            tempasset["verified"] = None
+        elif tempasset["verified"] == "0":
+            tempasset["verified"] = False
+        else:
+            tempasset["verified"] = True
+
+        if tempasset["has_static_ip"] == "":
+            tempasset["has_static_ip"] = None
+        elif tempasset["has_static_ip"] == "0":
+            tempasset["has_static_ip"] = False
+        else:
+            tempasset["has_static_ip"] = True
+
+        if tempasset["last_touch_date"] == "":
+            tempasset["last_touch_date"] = None
+        else:
+            tempasset["last_touch_date"] = parser.parse(tempasset["last_touch_date"])
+
+
+
+        to_add_asset = RepoAsset(**tempasset)
         db.session.add(to_add_asset)
+
+    # Adding preset Asset service connection
+    to_add_asset_services = import_fixture_from_file("repo_asset_repo_service_association_table")
+    for asset_service in to_add_asset_services:
+        service_to_link = RepoService.query.filter_by(id=asset_service["repo_service_id"]).first()
+        asset_to_link = RepoAsset.query.filter_by(id=asset_service["repo_asset_id"]).first()
+        service_to_link.assets.append(asset_to_link)
+
+        # to_add_asset_service = RepoAsset(**asset_service)
+        # db.session.add(to_add_asset_service)
 
     # Adding cves
     to_add_cves = import_fixture_from_file("common_vulnerabilities_and_exposures")
     for cve in to_add_cves:
-        to_add_cve = CommonVulnerabilitiesAndExposures(**cve)
+        print("-----------------TEMP IS -------------")
+        tempcve = cve
+        print(tempcve)
+        # cve = json.load(cve)
+        if tempcve["baseScore"] == "":
+            tempcve["baseScore"] = None
+        else:
+            tempcve["baseScore"] = float(tempcve["baseScore"])
+
+        if tempcve["exploitabilityScore"] == "":
+            tempcve["exploitabilityScore"] = None
+        else:
+            tempcve["exploitabilityScore"] = float(tempcve["exploitabilityScore"])
+
+        if tempcve["impactScore"] == "":
+            tempcve["impactScore"] = None
+        else:
+            tempcve["impactScore"] = float(tempcve["impactScore"])
+
+        if tempcve["obtainAllPrivilege"] == "":
+            tempcve["obtainAllPrivilege"] = None
+        elif tempcve["obtainAllPrivilege"] == "0":
+            tempcve["obtainAllPrivilege"] = False
+        else:
+            tempcve["obtainAllPrivilege"] = True
+
+        if tempcve["obtainUserPrivilege"] == "":
+            tempcve["obtainUserPrivilege"] = None
+        elif tempcve["obtainUserPrivilege"] == "0":
+            tempcve["obtainUserPrivilege"] = False
+        else:
+            tempcve["obtainUserPrivilege"] = True
+
+        if tempcve["obtainOtherPrivilege"] == "":
+            tempcve["obtainOtherPrivilege"] = None
+        elif tempcve["obtainOtherPrivilege"] == "0":
+            tempcve["obtainOtherPrivilege"] = False
+        else:
+            tempcve["obtainOtherPrivilege"] = True
+
+        if tempcve["userInteractionRequired"] == "":
+            tempcve["userInteractionRequired"] = None
+        elif tempcve["userInteractionRequired"] == "0":
+            tempcve["userInteractionRequired"] = False
+        else:
+            tempcve["userInteractionRequired"] = True
+
+        to_add_cve = CommonVulnerabilitiesAndExposures(**tempcve)
         db.session.add(to_add_cve)
 
     # Adding vulnerabilities reports
     to_add_vulnerability_reports = import_fixture_from_file("vulnerability_report")
     for vulnerability_report in to_add_vulnerability_reports:
-        to_add_vulnerability_report = VulnerabilityReport(**vulnerability_report)
+        temp_vulnerability_report = vulnerability_report
+
+
+        to_add_vulnerability_report = VulnerabilityReport(**temp_vulnerability_report)
         db.session.add(to_add_vulnerability_report)
 
     # Adding vulnerability reports link
     to_add_vulnerability_reports_links = import_fixture_from_file("vulnerability_report_vulnerabilities_link")
     for vulnerability_reports_link in to_add_vulnerability_reports_links:
+        temp_vulnerability_reports_link = vulnerability_reports_link
+
+        if temp_vulnerability_reports_link["date"] == "":
+            temp_vulnerability_reports_link["date"] = None
+        else:
+            temp_vulnerability_reports_link["date"] = parser.parse(temp_vulnerability_reports_link["date"])
+
+
         to_add_vulnerability_reports_link = VulnerabilityReportVulnerabilitiesLink(**vulnerability_reports_link)
         db.session.add(to_add_vulnerability_reports_link)
 
@@ -201,13 +295,29 @@ def rcra_db_init():
     # Adding repo risk threat asset materialisation
     to_add_asset_materialisations = import_fixture_from_file("repo_risk_threat_asset_materialisation")
     for assets_materialisation in to_add_asset_materialisations:
+        temp_assets_materialisation = assets_materialisation
+
+        # Convert Boolean value from json boolean to python boolean
+        if temp_assets_materialisation["threat_occurrence"] == "0":
+            temp_assets_materialisation["threat_occurrence"] = False
+        else:
+            temp_assets_materialisation["threat_occurrence"] = True
         to_add_asset_materialisation = RepoRiskThreatAssetMaterialisation(**assets_materialisation)
         db.session.add(to_add_asset_materialisation)
 
     # Adding repo risk threat asset consequence
     to_add_asset_consequences = import_fixture_from_file("repo_risk_threat_asset_consequence")
     for assets_consequence in to_add_asset_consequences:
-        to_add_asset_consequence = RepoRiskThreatAssetConsequence(**assets_consequence)
+        temp_assets_consequence = assets_consequence
+        if temp_assets_consequence["threat_occurrence"] == "0":
+            temp_assets_consequence["threat_occurrence"] = False
+        else:
+            temp_assets_consequence["threat_occurrence"] = True
+
+        to_add_asset_consequence = RepoRiskThreatAssetConsequence(**temp_assets_consequence)
+
+
+
         db.session.add(to_add_asset_consequence)
 
     # Adding repo objective impact relationship
@@ -221,5 +331,11 @@ def rcra_db_init():
     for asset_threat_consequence_service_impact in to_add_asset_threat_consequence_service_impacts:
         to_add_asset_threat_consequence_service_impact = RepoAssetThreatConsequenceServiceImpactRelationship(**asset_threat_consequence_service_impact)
         db.session.add(to_add_asset_threat_consequence_service_impact)
+
+    # Adding repo utility objectives relatinship values
+    to_add_utility_objective_relationships = import_fixture_from_file("repo_utility_objective_relationship")
+    for utility_objective_relationship in to_add_utility_objective_relationships:
+        to_add_utility_objective_relationship = RepoUtilityObjectiveRelationship(**utility_objective_relationship)
+        db.session.add(to_add_utility_objective_relationship)
 
     db.session.commit()
